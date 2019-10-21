b979448f9f9576cb1d49bcba3d7a95025f3e783d
João Lima <joao_vitor2722@hotmail.com>
2019-10-21 15:22:50 -0300

    Mudança do index
    Quase finalizada a mudança do index

--- Aldeia_V4.0/assets/js/core.js
+++ Aldeia_V4.0/assets/js/core.js
@@ -436,14 +436,14 @@ var Core = {
             });
 
         },
-        map: function() {
+        map: function initMap() {
 
             var $googleMap = $('#google-map');
 
             if($googleMap.length) {
 
-                var yourLatitude = 40.758895;   
-                var yourLongitude = -73.985131;    
+                var yourLatitude = -15.7761724;   
+                var yourLongitude = -47.8880109;    
 
                 var pickedStyle = $googleMap.data('style');     
                 var wy = [{"featureType":"all","elementType":"geometry.fill","stylers":[{"weight":"2.00"}]},{"featureType":"all","elementType":"geometry.stroke","stylers":[{"color":"#9c9c9c"}]},{"featureType":"all","elementType":"labels.text","stylers":[{"visibility":"on"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"landscape","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"color":"#eeeeee"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#7b7b7b"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#46bcec"},{"visibility":"on"}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#c8d7d4"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"color":"#070707"}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]}];
@@ -452,7 +452,7 @@ var Core = {
 
                 var mapOptions = {
                     zoom: 15,
-                    center: {lat: yourLatitude, lng: yourLongitude},
+                    center: {lat: -15.7761724, lng: -47.8880109},
                     mapTypeControl: false,
                     panControl: false,
                     zoomControl: true,
@@ -466,7 +466,7 @@ var Core = {
                 var myLatLng = new google.maps.LatLng(yourLatitude,yourLongitude);
                 var image = {
                     url: 'assets/img/location-pin.png',
-                    anchor: new google.maps.Point(79, 115),
+                    
                 };
                 var myLocation = new google.maps.Marker({
                     position: myLatLng,
@@ -474,6 +474,7 @@ var Core = {
                     icon: image
                 });
             }
+
 
         },
         plan: function() {
